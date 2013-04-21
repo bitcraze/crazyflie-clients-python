344a7cd8a67aa84bf4c872050a928cce6a065237
Arnaud Taffanel <unknown>
2013-04-21 14:47:00 +0200

    setup.py: Added experimental py2exe suport

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import glob
+
+try:
+    import py2exe
+except ImportError:
+    print("Warning: py2exe not usable")
 
 setup(name='cfclient',
       description='Bitcraze Cazyflie nano quadcopter client',
@@ -15,5 +21,22 @@ setup(name='cfclient',
                 'cflib.utils', 'cflib.crtp',
                ],
       scripts=['bin/cfclient'],
+      
+      #Py2exe specifics
+      console=['bin/cfclient'],
+      data_files=[('cfclient/ui', glob.glob('lib/cfclient/ui/*.ui')),
+                  ('cfclient/ui', glob.glob('lib/cfclient/ui/*.py')),
+                  ('cfclient/ui/tabs', glob.glob('lib/cfclient/ui/tabs/*.ui')),
+                  ('cfclient/ui/tabs', glob.glob('lib/cfclient/ui/tabs/*.py')),
+                  ('cfclient/ui/widgets', glob.glob('lib/cfclient/ui/widgets/*.ui')),
+                  ('cfclient/ui/widgets', glob.glob('lib/cfclient/ui/widgets/*.py')),
+                  ('cfclient/configs', glob.glob('lib/cfclient/configs/*.json')),
+                  ('cfclient/configs', glob.glob('lib/cfclient/configs/*.json')),
+                  ('cfclient/configs/input', glob.glob('lib/cfclient/configs/input/*.json')),
+                  ('cfclient/configs/log', glob.glob('lib/cfclient/configs/log/*.json')),
+                  ],
+      options={"py2exe" : {"includes" : ["sip", "PyQt4", "cfclient.ui.widgets", "cflib.bootloader.cloader"],
+                           "skip_archive": True,
+                          }}
      )
 
