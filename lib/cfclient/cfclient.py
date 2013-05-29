a92b539c5c9f37a55708273b1d559b06659b9592
Daniel Lee <unknown>
2013-05-29 07:07:47 +0200

    Clearer file docstring
    The previous docstring was a repeat of the one for lib/ui/main.py, which IMHO was a bit confusing.

--- lib/cfclient/cfclient.py
+++ lib/cfclient/cfclient.py
@@ -24,9 +24,7 @@
 #  this program; if not, write to the Free Software Foundation, Inc., 51
 #  Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-"""
-The main file for the Crazyflie control application.
-"""
+"""Initialization of the PC Client GUI."""
 
 __author__ = 'Bitcraze AB'
 __all__ = ['']
