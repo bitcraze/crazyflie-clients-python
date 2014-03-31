23bfded37c27d2cf9864fe236c83e57741fde720
Marcus Eliasson <marcus@bitcraze.se>
2014-03-31 11:10:01 +0200

    Support for multiple input-devices with the same name. Closes #94.

--- lib/cfclient/utils/pygamereader.py
+++ lib/cfclient/utils/pygamereader.py
@@ -132,9 +132,14 @@ class PyGameReader():
         dev = []
         pygame.joystick.quit()
         pygame.joystick.init()
+        names = []
         nbrOfInputs = pygame.joystick.get_count()
         for i in range(0,nbrOfInputs):
             j = pygame.joystick.Joystick(i)
-            dev.append({"id":i, "name" : j.get_name()})
+            name = j.get_name()
+            if names.count(name) > 0:
+                name = "{0} #{1}".format(name, names.count(name) + 1)
+            dev.append({"id":i, "name" : name})
+            names.append(name)
         return dev
 
