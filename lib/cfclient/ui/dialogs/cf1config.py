8582e94994f0fc7e50d8e6ef57fef28bb287abbc
Marcus Eliasson <marcus@bitcraze.se>
2015-01-16 12:00:05 +0100

    Fixed broken CF1 config/bootloader. Closes #135

--- lib/cfclient/ui/dialogs/cf1config.py
+++ lib/cfclient/ui/dialogs/cf1config.py
@@ -35,7 +35,7 @@ __all__ = ['Cf1ConfigDialog']
 
 import struct
 import sys
-import time
+from cflib.bootloader import Bootloader
 
 import logging
 logger = logging.getLogger(__name__)
@@ -43,16 +43,10 @@ logger = logging.getLogger(__name__)
 from PyQt4 import QtCore, QtGui, uic
 from PyQt4.QtCore import Qt, pyqtSlot, pyqtSignal, QThread, SIGNAL
 
-from cfclient.ui.tab import Tab
-
-import cflib.crtp
-
-from cflib.bootloader.cloader import Cloader
 from cfclient.utils.guiconfig import GuiConfig
 
 service_dialog_class = uic.loadUiType(sys.path[0] +
                                       "/cfclient/ui/dialogs/cf1config.ui")[0]
-
 
 class UIState:
     DISCONNECTED = 0
@@ -85,12 +79,7 @@ class Cf1ConfigDialog(QtGui.QWidget, service_dialog_class):
         self.saveConfigblock.clicked.connect(self.writeConfig)
         self._cancel_bootloading.clicked.connect(self.close)
 
-        # connecting other signals
-        #self.clt.programmed.connect(self.programDone)
-        #self.clt.verified.connect(self.verifyDone)
         self.clt.statusChanged.connect(self.statusUpdate)
-        # self.clt.updateBootloaderStatusSignal.connect(
-        #                                         self.updateBootloaderStatus)
         self.clt.connectingSignal.connect(lambda:
                                           self.setUiState(UIState.CONNECTING))
         self.clt.connectedSignal.connect(lambda:
@@ -99,8 +88,6 @@ class Cf1ConfigDialog(QtGui.QWidget, service_dialog_class):
         self.clt.disconnectedSignal.connect(lambda:
                                         self.setUiState(UIState.DISCONNECTED))
         self.clt.updateConfigSignal.connect(self.updateConfig)
-        self.clt.updateCpuIdSignal.connect(lambda cpuid:
-                                           self.copterId.setText(cpuid))
 
         self.clt.start()
 
@@ -138,7 +125,6 @@ class Cf1ConfigDialog(QtGui.QWidget, service_dialog_class):
             self.pitchTrim.setValue(0)
             self.radioChannel.setValue(0)
             self.radioSpeed.setCurrentIndex(0)
-            self.copterId.setText("")
 
     def setStatusLabel(self, text):
         self.connectionStatus.setText("Status: <b>%s</b>" % text)
@@ -167,13 +153,13 @@ class Cf1ConfigDialog(QtGui.QWidget, service_dialog_class):
     def statusUpdate(self, status, progress):
         logger.debug("Status: [%s] | %d", status, progress)
         self.statusLabel.setText('Status: <b>' + status + '</b>')
-        if progress >= 0:
+        if progress >= 0 and progress < 100:
             self.progressBar.setTextVisible(True)
             self.progressBar.setValue(progress)
         else:
             self.progressBar.setTextVisible(False)
             self.progressBar.setValue(100)
-        if (progress == 100):
+        if progress >= 100:
             self.resetButton.setEnabled(True)
 
     def initiateColdboot(self):
@@ -192,38 +178,28 @@ class Cf1ConfigDialog(QtGui.QWidget, service_dialog_class):
 # event loop which is what we want
 class CrazyloadThread(QThread):
     # Input signals declaration (not sure it should be used like that...)
-    program = pyqtSignal(str, bool)
-    verify = pyqtSignal()
     initiateColdBootSignal = pyqtSignal(str)
     resetCopterSignal = pyqtSignal()
     writeConfigSignal = pyqtSignal(int, int, float, float)
     # Output signals declaration
-    programmed = pyqtSignal()
-    verified = pyqtSignal()
     statusChanged = pyqtSignal(str, int)
     connectedSignal = pyqtSignal()
     connectingSignal = pyqtSignal()
     failed_signal = pyqtSignal(str)
     disconnectedSignal = pyqtSignal()
     updateConfigSignal = pyqtSignal(int, int, float, float)
-    updateCpuIdSignal = pyqtSignal(str)
-
-    radioSpeedPos = 2
 
     def __init__(self):
         super(CrazyloadThread, self).__init__()
 
         # Make sure that the signals are handled by this thread event loop
         self.moveToThread(self)
+        self._bl = Bootloader()
+        self._bl.progress_cb = self.statusChanged.emit
 
-        self.program.connect(self.programAction)
         self.writeConfigSignal.connect(self.writeConfigAction)
         self.initiateColdBootSignal.connect(self.initiateColdBoot)
         self.resetCopterSignal.connect(self.resetCopter)
-        self.loader = None
-        self.link = None
-
-        self._config_page = None
 
     def __del__(self):
         self.quit()
@@ -231,37 +207,16 @@ class CrazyloadThread(QThread):
 
     def initiateColdBoot(self, linkURI):
         self.connectingSignal.emit()
+
         try:
-            self.link = cflib.crtp.get_link_driver("radio://0/110")
-        except Exception as e:
-            self.failed_signal.emit(str(e))
-
-        if (self.link):
-            self.loader = Cloader(self.link, "radio://0/110")
-
-            if self.loader.coldboot():
-                logger.info("Connected in coldboot mode ok")
-                self.updateCpuIdSignal.emit(self.loader.cpuid)
-                self._config_page = self.loader.flash_pages-self.loader.start_page-1
-                logger.debug("Config page {} pages after the bootloader".format(self._config_page))
-                self.readConfigAction()
-                self.connectedSignal.emit()
+            success = self._bl.start_bootloader(warm_boot=False)
+            if not success:
+                self.failed_signal.emit("Could not connect to bootloader")
             else:
-                self.failed_signal.emit("Connection timeout")
-                logger.info("Connected in coldboot mode failed")
-                self.link.close()
-
-    def programAction(self, filename, verify):
-        logger.info("Flashing file [%s]", filename)
-        f = open(filename, "rb")
-        if not f:
-            logger.warning("Cannot open file [%s]", filename)
-            self.link.close()
-            return
-        image = f.read()
-        f.close()
-
-        self.loadAndFlash(image, verify)
+                self.connectedSignal.emit()
+                self.readConfigAction()
+        except Exception as e:
+            self.failed_signal.emit("{}".format(e))
 
     def checksum256(self, st):
         return reduce(lambda x, y: x + y, map(ord, st)) % 256
@@ -273,116 +228,33 @@ class CrazyloadThread(QThread):
         image = "0xBC" + image
         image += struct.pack("B", 256 - self.checksum256(image))
 
-        self.loadAndFlash(image, True, self._config_page)
+        self._bl.write_cf1_config(image)
 
     def readConfigAction(self):
         self.statusChanged.emit("Reading config block...", 0)
-        data = self.loader.read_flash(self.loader.start_page + self._config_page)
+        data = self._bl.read_cf1_config()
         if (data is not None):
-            self.statusChanged.emit("Reading config block...done!", 100)
             if data[0:4] == "0xBC":
                 # Skip 0xBC and version at the beginning
                 [channel,
                  speed,
                  pitchTrim,
                  rollTrim] = struct.unpack("<BBff", data[5:15])
+                self.statusChanged.emit("Reading config block...done!", 100)
             else:
                 channel = GuiConfig().get("default_cf_channel")
                 speed = GuiConfig().get("default_cf_speed")
                 pitchTrim = GuiConfig().get("default_cf_trim")
                 rollTrim = GuiConfig().get("default_cf_trim")
+                self.statusChanged.emit("Could not find config block, showing defaults", 100)
             self.updateConfigSignal.emit(channel, speed, rollTrim, pitchTrim)
         else:
             self.statusChanged.emit("Reading config block failed!", 0)
 
-    def loadAndFlash(self, image, verify=False, startpage=0):
-
-        factor = ((100.0 * self.loader.page_size) / len(image))
-        if (verify == True):
-            factor /= 2
-        progress = 0
-        # For each page
-        ctr = 0  # Buffer counter
-        for i in range(0, int((len(image) - 1) / self.loader.page_size) + 1):
-            # Load the buffer
-            if ((i + 1) * self.loader.page_size) > len(image):
-                self.loader.upload_buffer(ctr, 0,
-                                          image[i * self.loader.page_size:])
-            else:
-                self.loader.upload_buffer(ctr, 0,
-                                          image[i *
-                                                self.loader.page_size:(i + 1) *
-                                                self.loader.page_size])
-
-            ctr += 1
-
-            progress += factor
-            self.statusChanged.emit("Uploading buffer...", int(progress))
-
-            # Flash when the complete buffers are full
-            if ctr >= self.loader.buffer_pages:
-                self.statusChanged.emit("Writing buffer...", int(progress))
-                firstFlashPage = (self.loader.start_page + startpage + i -
-                                  (ctr - 1))
-                if not self.loader.write_flash(0, firstFlashPage, ctr):
-                    self.disconnectedSignal.emit()
-                    self.statusChanged.emit("Error during flash operation "
-                                            "(err code %d)" %
-                                            self.loader.error_code,
-                                            int(progress))
-                    self.link.close()
-                    return
-                if (verify == True):
-                    for p in range(firstFlashPage, firstFlashPage + ctr):
-                        test = self.loader.read_flash(p)
-                        buffStart = ((p - self.loader.start_page) *
-                                     self.loader.page_size)
-                        ver = image[buffStart:buffStart +
-                                    self.loader.page_size]
-                        if (test != ver):
-                            self.statusChanged.emit("Verification failed!",
-                                                    int(progress))
-                            return
-                        progress += factor
-                        self.statusChanged.emit("Verifying flashed data...",
-                                                int(progress))
-
-                ctr = 0
-
-        if ctr > 0:
-            self.statusChanged.emit("Writing buffer...", int(progress))
-            firstFlashPage = (self.loader.start_page + startpage +
-                              (int((len(image) - 1) / self.loader.page_size)) -
-                              (ctr - 1))
-            if not self.loader.write_flash(0, firstFlashPage, ctr):
-                self.statusChanged.emit("Error during flash operation "
-                                        "(err code %d)" %
-                                        self.loader.error_code,
-                                        int(progress))
-                self.disconnectedSignal.emit()
-                self.link.close()
-                return
-            if (verify == True):
-                for p in range(firstFlashPage, firstFlashPage + ctr):
-                    buffStart = ((p - self.loader.start_page) *
-                                 self.loader.page_size)
-                    ver = image[buffStart:buffStart + self.loader.page_size]
-                    # We read back more than we should compare.
-                    test = self.loader.read_flash(p)[0:len(ver)]
-                    if (test != ver):
-                        self.statusChanged.emit("Verification failed!",
-                                                int(progress))
-                        return
-                    progress += factor
-                    self.statusChanged.emit("Verifying flashed data...",
-                                            int(progress))
-
-        self.statusChanged.emit("Flashing...done!", 100)
-
     def resetCopter(self):
+        try:
+            self._bl.reset_to_firmware()
+        except Exception:
+            pass
+        self._bl.close()
         self.disconnectedSignal.emit()
-        if self.loader:
-            self.loader.reset_to_firmware(self.loader.decode_cpu_id(
-                                        "32:00:6e:06:58:37:35:32:60:58:01:43"))
-        if self.link:
-            self.link.close()
