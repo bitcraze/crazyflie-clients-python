2d27e82e1061a520a92cf80b025ebb09ec1ea27f
Marcus Eliasson <marcus@bitcraze.se>
2015-03-17 11:34:02 +0100

    Modified PySDL2 to fit new input arch

--- lib/cfclient/utils/inputreaders/pysdl2.py
+++ lib/cfclient/utils/inputreaders/pysdl2.py
@@ -47,23 +47,18 @@ logger = logging.getLogger(__name__)
 MODULE_MAIN = "PySDL2Reader"
 MODULE_NAME = "PySDL2"
 
-class PySDL2Reader():
-    """Used for reading data from input devices using the PySDL2 API."""
-    def __init__(self):
-        self._j = None
-        sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO | sdl2.SDL_INIT_JOYSTICK)
-        sdl2.SDL_SetHint(sdl2.hints.SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1")
-        sdl2.ext.init()
+class _JS():
+    def __init__(self, num, name):
         self.axes = []
         self.buttons = []
         self.name = MODULE_NAME
+        self._j = None
         self._btn_count = 0
+        self._id = num
+        self._name = name
 
-
-    def open(self, deviceId):
-        """Initalize the reading and open the device with deviceId and set the mapping for axis/buttons using the
-        inputMap"""
-        self._j = sdl2.SDL_JoystickOpen(deviceId)
+    def open(self):
+        self._j = sdl2.SDL_JoystickOpen(self._id)
         self._btn_count = sdl2.SDL_JoystickNumButtons(self._j)
 
         self.axes = list(0 for i in range(sdl2.SDL_JoystickNumAxes(self._j)))
@@ -82,8 +77,6 @@ class PySDL2Reader():
             self.buttons[self._btn_count+btn] = 1
 
     def read(self):
-        """Read input from the selected device."""
-
         for e in sdl2.ext.get_events():
             if e.type == sdl2.SDL_JOYAXISMOTION:
                 self.axes[e.jaxis.axis] = e.jaxis.value / 32767.0
@@ -105,16 +98,34 @@ class PySDL2Reader():
                     self._set_fake_hat_button(2)
                 elif e.jhat.value == sdl2.SDL_HAT_RIGHT:
                     self._set_fake_hat_button(3)
-
         return [self.axes, self.buttons]
+
+class PySDL2Reader():
+    """Used for reading data from input devices using the PySDL2 API."""
+    def __init__(self):
+        sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO | sdl2.SDL_INIT_JOYSTICK)
+        sdl2.SDL_SetHint(sdl2.hints.SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1")
+        sdl2.ext.init()
+        self._js = {}
+        self.name = MODULE_NAME
+
+    def open(self, device_id):
+        """Initalize the reading and open the device with deviceId and set the mapping for axis/buttons using the
+        inputMap"""
+        self._js[device_id].open()
+
+    def close(self, device_id):
+        self._js[device_id].close()
+
+    def read(self, device_id):
+        """Read input from the selected device."""
+        return self._js[device_id].read()
 
     def devices(self):
         """List all the available devices."""
         logger.info("Looking for devices")
         dev = []
         names = []
-        if hasattr(self, 'j') and sdl2.joystick.SDL_JoystickGetAttached(self._j):
-            sdl2.joystick.SDL_JoystickClose(self._j)
 
         nbrOfInputs = sdl2.joystick.SDL_NumJoysticks()
         for i in range(0, nbrOfInputs):
@@ -123,6 +134,7 @@ class PySDL2Reader():
             if names.count(name) > 0:
                 name = "{0} #{1}".format(name, names.count(name) + 1)
             dev.append({"id": i, "name": name})
+            self._js[i] = _JS(i, name)
             names.append(name)
             sdl2.joystick.SDL_JoystickClose(j)
         return dev
