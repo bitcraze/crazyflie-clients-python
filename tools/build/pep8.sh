3a425b26fb55c8df5f3608b6f06754cf4c9f6324
Kristoffer Richardsson <kristoffer@bitcraze.io>
2015-08-21 17:28:42 +0200

    Updates to PEP8

--- tools/build/pep8.sh
+++ tools/build/pep8.sh
@@ -3,8 +3,8 @@ set -e
 
 scriptDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# We will add more and more errors over time. We don't want them all at one time to avoid mental overload.
-included_errors="E231"
+# We will remove more and more errors over time. We don't want them all at one time to avoid mental overload.
+exclude_errors="E111,E114,E122,E123,E126,E127,E128,E129,E201,E202,E203,E211,E222,E225,E226,E227,E228,E231,E241,E251,E261,E262,E265,E266,E301,E302,E303,E401,E402,E501,E701,E703,E711,E712,E713,W291,W292,W293,W391,W503,W601"
 
-pep8 --filename=* --statistics --select=${included_errors} ${scriptDir}/../../bin
-pep8 --filename=**/bin/cfclient --statistics --select=${included_errors} ${scriptDir}/../..
+pep8 --statistics --filename="*" --ignore="${exclude_errors}" ${scriptDir}/../../bin
+pep8 --statistics --ignore=${exclude_errors} ${scriptDir}/../..
