facc1786c97bbd4573085707bdb0e2a516a604be
Arnaud Taffanel <arnaud@bitcraze.se>
2016-04-12 11:19:48 +0200

    Fixed windows build

--- setup.py
+++ setup.py
@@ -39,6 +39,10 @@ def get_version():
 
     return version
 
+
+def relative(lst, base=''):
+    return list(map(lambda x: base + os.path.basename(x), lst))
+
 VERSION = get_version()
 
 if not VERSION and not os.path.isfile('src/cfclient/version.json'):
@@ -61,9 +65,41 @@ if sys.platform == 'win32' or sys.platform == 'darwin':
 if sys.platform == 'win32':
     platform_dev_requires = ['py2exe', 'jinja2']
 
+# Make a special case when running py2exe to be able to access resources
+if sys.platform == 'win32' and sys.argv[1] == 'py2exe':
+    package_data = {}
+    data_files = [
+        ('', ['README.md', 'version.json']),
+        ('ui', glob('src/cfclient/ui/*.ui')),
+        ('ui/tabs', glob('src/cfclient/ui/tabs/*.ui')),
+        ('ui/widgets', glob('src/cfclient/ui/widgets/*.ui')),
+        ('ui/toolboxes', glob('src/cfclient/ui/toolboxes/*.ui')),
+        ('ui/dialogs', glob('src/cfclient/ui/dialogs/*.ui')),
+        ('configs', glob('src/cfclient/configs/*.json')),
+        ('configs/input', glob('src/cfclient/configs/input/*.json')),
+        ('configs/log', glob('src/cfclient/configs/log/*.json')),
+        ('', glob('src/cfclient/*.png')),
+        ('resources', glob('src/cfclient/resources/*')),
+        ('third_party', glob('src/cfclient/third_party/*')),
+    ]
+else:
+    package_data = {
+        'cfclient.ui':  relative(glob('src/cfclient/ui/*.ui')),
+        'cfclient.ui.tabs': relative(glob('src/cfclient/ui/tabs/*.ui')),
+        'cfclient.ui.widgets':  relative(glob('src/cfclient/ui/widgets/*.ui')),
+        'cfclient.ui.toolboxes':  relative(glob('src/cfclient/ui/toolboxes/*.ui')),  # noqa
+        'cfclient.ui.dialogs':  relative(glob('src/cfclient/ui/dialogs/*.ui')),
+        'cfclient':  relative(glob('src/cfclient/configs/*.json'), 'configs/') +  # noqa
+                     relative(glob('src/cfclient/configs/input/*.json'), 'configs/input/') +  # noqa
+                     relative(glob('src/cfclient/configs/log/*.json'), 'configs/log/') +  # noqa
+                     relative(glob('src/cfclient/resources/*'), 'resources/') +
+                     relative(glob('src/cfclient/*.png')),
+        '': ['README.md']
+    }
+    data_files = [
+        ('third_party', glob('src/cfclient/third_party/*')),
+    ],
 
-def relative(lst, base=''):
-    return list(map(lambda x: base + os.path.basename(x), lst))
 
 # Initial parameters
 setup(
@@ -105,19 +141,7 @@ setup(
         'dev': platform_dev_requires + []
     },
 
-    package_data={
-        'cfclient.ui':  relative(glob('src/cfclient/ui/*.ui')),
-        'cfclient.ui.tabs': relative(glob('src/cfclient/ui/tabs/*.ui')),
-        'cfclient.ui.widgets':  relative(glob('src/cfclient/ui/widgets/*.ui')),
-        'cfclient.ui.toolboxes':  relative(glob('src/cfclient/ui/toolboxes/*.ui')),  # noqa
-        'cfclient.ui.dialogs':  relative(glob('src/cfclient/ui/dialogs/*.ui')),
-        'cfclient':  relative(glob('src/cfclient/configs/*.json'), 'configs/') +  # noqa
-                     relative(glob('src/cfclient/configs/input/*.json'), 'configs/input/') +  # noqa
-                     relative(glob('src/cfclient/configs/log/*.json'), 'configs/log/') +  # noqa
-                     relative(glob('src/cfclient/resources/*'), 'resources/') +
-                     relative(glob('src/cfclient/*.png')),
-        '': ['README.md']
-    },
+    package_data=package_data,
 
     # Py2exe options
     console=[
@@ -126,14 +150,21 @@ setup(
             'icon_resources': [(0, 'bitcraze.ico')]
         }
     ],
-    py2exe={
-        'includes': ['cfclient.ui.widgets.hexspinbox'],
-        'bundle_files': 3,
-        'skip_archive': True,
+    options={
+        "py2exe": {
+            'includes': ['cfclient.ui.widgets.hexspinbox',
+                         'zmq.backend.cython'],
+            'bundle_files': 3,
+            'skip_archive': True,
+        },
     },
 
-    data_files=[
-        # ('', ['README.md']),
-        ('third_party', glob('src/cfclient/third_party/*')),
-    ],
+    data_files=data_files
 )
+
+# Fixing the zmq lib in the windows binary dist folder
+if sys.platform == 'win32' and sys.argv[1] == 'py2exe':
+    print("Renaming zmq dll")
+    if os.path.isfile('dist/zmq.libzmq.pyd') and \
+       not os.path.isfile('dist/libzmq.pyd'):
+        os.rename('dist/zmq.libzmq.pyd', 'dist/libzmq.pyd')
