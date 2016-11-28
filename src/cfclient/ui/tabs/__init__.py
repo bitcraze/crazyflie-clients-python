b5d15f21a69296594bb3e59e59b21fc190817776
Arnaud Taffanel <arnaud@bitcraze.se>
2016-11-28 09:02:17 +0100

    Disabled GPS tab
    The GPS tab is not working anymore (#266) and is causing
    problems for running the client on Ubuntu 16.10. Disabling
    it until we find a better solution for displaying a map.

--- src/cfclient/ui/tabs/__init__.py
+++ src/cfclient/ui/tabs/__init__.py
@@ -33,7 +33,7 @@ it into the UI when it is started.
 from .ConsoleTab import ConsoleTab
 # from .ExampleTab import ExampleTab
 from .FlightTab import FlightTab
-from .GpsTab import GpsTab
+# from .GpsTab import GpsTab
 from .LEDTab import LEDTab
 from .LogBlockTab import LogBlockTab
 from .LogTab import LogTab
@@ -47,7 +47,7 @@ available = [
     ConsoleTab,
     # ExampleTab,
     FlightTab,
-    GpsTab,
+#     GpsTab,
     LEDTab,
     LogBlockTab,
     LogTab,
