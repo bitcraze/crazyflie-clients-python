a332bddae2efade54f08d180d9fe05cc87836ec2
Kristoffer Richardsson <kristoffer@bitcraze.io>
2017-03-16 15:20:59 +0100

    #269 Reverted fix for repaint problems in qt4 on MacOS (#260)

--- src/cfclient/ui/main.py
+++ src/cfclient/ui/main.py
@@ -35,7 +35,6 @@ import cfclient.ui.toolboxes
 import cflib.crtp
 from cfclient.ui.dialogs.about import AboutDialog
 from cfclient.ui.dialogs.bootloader import BootloaderDialog
-from cfclient.ui.widgets.plotwidget import PlotWidget
 from cfclient.utils.config import Config
 from cfclient.utils.config_manager import ConfigManager
 from cfclient.utils.input import JoystickReader
@@ -58,11 +57,6 @@ from PyQt5.QtGui import QDesktopServices
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QMenu
 from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtWidgets import QTreeView
-from PyQt5.QtWidgets import QTreeWidget
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QTextEdit
-from PyQt5.QtCore import QTimer
 
 from .dialogs.cf1config import Cf1ConfigDialog
 from .dialogs.cf2config import Cf2ConfigDialog
@@ -383,38 +377,6 @@ class MainUI(QtWidgets.QMainWindow, main_window_class):
             node.setData((m, mux_subnodes))
 
         self._mapping_support = True
-
-        # Fix for issue #260: a long-known bug related to pyqtgraph on OSX
-        # caused UI repaint to fail sometimes (eg: after connecting to a CF).
-        # Uninstalling pyqtgraph would solve the issue, but this library may
-        # be needed for other functionality.
-        # As a workaround, we manually force a window repaint with a timer.
-        self.timer_repaint = QTimer()
-        self.timer_repaint.timeout.connect(self._repaint_UI)
-        if sys.platform == 'darwin':  # Bug has only been reported on OSX
-            self.timer_repaint.start(500)  # T=500ms, so CPU doesn't skyrocket
-
-    @pyqtSlot()
-    def _repaint_UI(self):
-        self.update()  # Qt optimizs performance better with update vs repaint
-
-        # (Fix for issue #260). For some reason, certain widgets (param list,
-        # plotter, log TOC, GPS widget, console output...) in the active tab
-        # still won't get repainted after calling self.update(). Even calling
-        # update() or repaint() on each individual widget doesn't work.
-        # However, toggling the widget visibility twice seems to do the trick!
-        for i in self.tabs.currentWidget().children():
-            if type(i) is QTreeView or \
-                            type(i) is PlotWidget or \
-                            type(i) is QTreeWidget or \
-                            type(i) is QWidget or \
-                            type(i) is QTextEdit:
-                # i.hide() followed by a i.show() accomplishes the same effect
-                # however that way, the selected widget in the active tab
-                # loses focus (very annoying when manually setting the range
-                # in the Plotter tab through the QSpinBoxes, for example).
-                i.resize(i.width()-1, i.height()-1)  # Make slightly smaller
-                i.resize(i.width()+1, i.height()+1)  # & back to original size
 
     def interfaceChanged(self, interface):
         if interface == INTERFACE_PROMPT_TEXT:
