1e94e2b0a483c5d140b9c70a23dbd425656a9e47
victor <victorkrook96@gmail.com>
2020-08-11 15:46:18 +0200

    Fixed naive filepath for CONTEXT.txt file

--- src/cfclient/gui.py
+++ src/cfclient/gui.py
@@ -158,7 +158,10 @@ def main():
             pass
 
     main_window = MainUI()
+    app.setStyleSheet(UiUtils.progressbar_stylesheet(UiUtils.COLOR_BLUE))
+    app.setFont(UiUtils.FONT)
     main_window.show()
+    main_window.set_default_theme()
     sys.exit(app.exec_())
 
 
