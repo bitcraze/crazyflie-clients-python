46a2a30df1f01558adaf64a85a4532f89229ee55
Kristoffer Richardsson <kristoffer@bitcraze.io>
2020-08-18 15:11:35 +0200

    Updated pysdl2 requireent. Closes #425

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ else:
 platform_requires = []
 platform_dev_requires = []
 if sys.platform == 'win32' or sys.platform == 'darwin':
-    platform_requires = ['pysdl2==0.9.6']
+    platform_requires = ['pysdl2~=0.9.6']
 if sys.platform == 'win32':
     platform_dev_requires = ['cx_freeze==5.1.1', 'jinja2==2.10.3']
 
