94504c1b6de85d0695b84590ceb6b074f66e5c6d
Kristoffer Richardsson <kristoffer@bitcraze.io>
2021-02-18 14:00:35 +0100

    #468 Moved geo estimation functionality to the lib

--- src/cfclient/ui/dialogs/lighthouse_bs_geometry_dialog.py
+++ src/cfclient/ui/dialogs/lighthouse_bs_geometry_dialog.py
@@ -31,8 +31,8 @@ import cfclient
 from PyQt5 import QtWidgets
 from PyQt5 import uic
 from PyQt5.QtCore import QVariant, Qt, QAbstractTableModel, pyqtSignal
-from cflib.localization.lighthouse_bs_vector import LighthouseBsVector
-from cflib.localization.lighthouse_bs_geo import LighthouseBsGeoEstimator
+from cflib.localization import LighthouseBsGeoEstimator
+from cflib.localization import LighthouseSweepAngleAverageReader
 from cflib.crazyflie.mem import LighthouseBsGeometry
 
 __author__ = 'Bitcraze AB'
@@ -44,125 +44,6 @@ logger = logging.getLogger(__name__)
     uic.loadUiType(
         cfclient.module_path + '/ui/dialogs/lighthouse_bs_geometry_dialog.ui')
 )
-
-
-class LighthouseSweepAngleReader():
-    ANGLE_STREAM_PARAM = 'locSrv.enLhAngleStream'
-    NR_OF_SENSORS = 4
-
-    def __init__(self, cf, data_recevied_cb):
-        self._cf = cf
-        self._cb = data_recevied_cb
-        self._is_active = False
-
-    def start(self):
-        self._cf.loc.receivedLocationPacket.add_callback(self._packet_received_cb)
-        self._angle_stream_activate(True)
-        self._is_active = True
-
-    def stop(self):
-        if self._is_active:
-            self._is_active = False
-            self._cf.loc.receivedLocationPacket.remove_callback(self._packet_received_cb)
-            self._angle_stream_activate(False)
-
-    def _angle_stream_activate(self, is_active):
-        value = 0
-        if is_active:
-            value = 1
-        self._cf.param.set_value(self.ANGLE_STREAM_PARAM, value)
-
-    def _packet_received_cb(self, packet):
-        if packet.type != self._cf.loc.LH_ANGLE_STREAM:
-            return
-
-        if self._cb:
-            base_station_id = packet.data["basestation"]
-            horiz_angles = packet.data['x']
-            vert_angles = packet.data['y']
-
-            result = []
-            for i in range(self.NR_OF_SENSORS):
-                result.append(LighthouseBsVector(horiz_angles[i], vert_angles[i]))
-
-            self._cb(base_station_id, result)
-
-
-class LighthouseSweepAngleAverageReader():
-    def __init__(self, cf, ready_cb):
-        self._reader = LighthouseSweepAngleReader(cf, self._data_recevied_cb)
-        self._ready_cb = ready_cb
-        self.nr_of_samples_required = 50
-
-        # We store all samples in the storage for averaging when data is collected
-        # The storage is a dictionary keyed on the base station channel
-        # Each entry is a list of 4 lists, one per sensor.
-        # Each list contains LighthouseBsVector objects, representing the sampled sweep angles
-        self._sample_storage = None
-
-    def start_angle_collection(self):
-        self._sample_storage = {}
-        self._reader.start()
-
-    def stop_angle_collection(self):
-        self._reader.stop()
-        self._sample_storage = None
-
-    def is_collecting(self):
-        return self._sample_storage is not None
-
-    def _data_recevied_cb(self, base_station_id, bs_vectors):
-        self._store_sample(base_station_id, bs_vectors, self._sample_storage)
-        if self._has_collected_enough_data(self._sample_storage):
-            self._reader.stop()
-            if self._ready_cb:
-                averages = self._average_all_lists(self._sample_storage)
-                self._ready_cb(averages)
-            self._sample_storage = None
-
-    def _store_sample(self, base_station_id, bs_vectors, storage):
-        if base_station_id not in storage:
-            storage[base_station_id] = []
-            for sensor in range(self._reader.NR_OF_SENSORS):
-                storage[base_station_id].append([])
-
-        for sensor in range(self._reader.NR_OF_SENSORS):
-            storage[base_station_id][sensor].append(bs_vectors[sensor])
-
-    def _has_collected_enough_data(self, storage):
-        for sample_list in storage.values():
-            if len(sample_list[0]) >= self.nr_of_samples_required:
-                return True
-        return False
-
-    def _average_all_lists(self, storage):
-        result = {}
-
-        for id, sample_lists in storage.items():
-            averages = self._average_sample_lists(sample_lists)
-            count = len(sample_lists[0])
-            result[id] = (count, averages)
-
-        return result
-
-    def _average_sample_lists(self, sample_lists):
-        result = []
-
-        for i in range(self._reader.NR_OF_SENSORS):
-            result.append(self._average_sample_list(sample_lists[i]))
-
-        return result
-
-    def _average_sample_list(self, sample_list):
-        sum_horiz = 0.0
-        sum_vert = 0.0
-
-        for bs_vector in sample_list:
-            sum_horiz += bs_vector.lh_v1_horiz_angle
-            sum_vert += bs_vector.lh_v1_vert_angle
-
-        count = len(sample_list)
-        return LighthouseBsVector(sum_horiz / count, sum_vert / count)
 
 
 class LighthouseBsGeometryTableModel(QAbstractTableModel):
