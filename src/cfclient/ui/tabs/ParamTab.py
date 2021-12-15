0aafb3e4ba8403afd7e70b8822a97c7323754fc5
Jonas Danielsson <jonas@bitcraze.io>
2021-12-15 14:10:05 +0100

    ParamTab: Add side panel with details
    That also includes persistent state

--- src/cfclient/ui/tabs/ParamTab.py
+++ src/cfclient/ui/tabs/ParamTab.py
@@ -53,37 +53,23 @@ logger = logging.getLogger(__name__)
 class ParamChildItem(object):
     """Represents a leaf-node in the tree-view (one parameter)"""
 
-    def __init__(self, parent, name, crazyflie):
+    def __init__(self, parent, name, persistent, crazyflie):
         """Initialize the node"""
         self.parent = parent
         self.name = name
         self.ctype = None
         self.access = None
+        self.persistent = False
         self.value = ""
-        self.description = str()
         self._cf = crazyflie
         self.is_updating = True
-
-        if cfclient.log_param_doc is not None:
-            try:
-                group = cfclient.log_param_doc['params'][parent.name]
-                self.description = group['variables'][name]['short_desc']
-            except:  # noqa
-                pass
+        self.state = None
 
     def updated(self, name, value):
         """Callback from the param layer when a parameter has been updated"""
         self.value = value
         self.is_updating = False
         self.parent.model.refresh()
-
-    def set_value(self, value):
-        """Send the update value to the Crazyflie. It will automatically be
-        read again after sending and then the updated callback will be
-        called"""
-        complete_name = "%s.%s" % (self.parent.name, self.name)
-        self._cf.param.set_value(complete_name, value)
-        self.is_updating = True
 
     def child_count(self):
         """Return the number of children this node has"""
@@ -100,14 +86,6 @@ class ParamGroupItem(object):
         self.children = []
         self.name = name
         self.model = model
-        self.description = str()
-
-        if cfclient.log_param_doc is not None:
-            try:
-                group = cfclient.log_param_doc['params'][name]
-                self.description = group['desc']
-            except:  # noqa
-                pass
 
     def child_count(self):
         """Return the number of children this node has"""
@@ -121,7 +99,7 @@ class ParamBlockModel(QAbstractItemModel):
         """Create the empty model"""
         super(ParamBlockModel, self).__init__(parent)
         self._nodes = []
-        self._column_headers = ['Name', 'Type', 'Access', 'Value', 'Description']
+        self._column_headers = ['Name', 'Type', 'Access', 'Persistent', 'Value']
         self._red_brush = QBrush(QColor("red"))
         self._enabled = False
 
@@ -137,9 +115,13 @@ class ParamBlockModel(QAbstractItemModel):
         for group in sorted(toc.keys()):
             new_group = ParamGroupItem(group, self)
             for param in sorted(toc[group].keys()):
-                new_param = ParamChildItem(new_group, param, crazyflie)
-                new_param.ctype = toc[group][param].ctype
-                new_param.access = toc[group][param].get_readable_access()
+                elem = toc[group][param]
+                is_persistent = elem.is_persistent()
+                new_param = ParamChildItem(new_group, param, is_persistent, crazyflie)
+                new_param.ctype = elem.ctype
+                new_param.access = elem.get_readable_access()
+                new_param.persistent = elem.is_persistent()
+
                 crazyflie.param.add_update_callback(
                     group=group, name=param, cb=new_param.updated)
                 new_group.children.append(new_param)
@@ -208,8 +190,6 @@ class ParamBlockModel(QAbstractItemModel):
         if not parent:
             if role == Qt.DisplayRole and index.column() == 0:
                 return node.name
-            if role == Qt.DisplayRole and index.column() == 4:
-                return node.description
         elif role == Qt.DisplayRole:
             if index.column() == 0:
                 return node.name
@@ -218,27 +198,14 @@ class ParamBlockModel(QAbstractItemModel):
             if index.column() == 2:
                 return node.access
             if index.column() == 3:
-                return node.value
+                return 'Yes' if node.persistent else 'No'
             if index.column() == 4:
-                return node.description
-        elif role == Qt.EditRole and index.column() == 3:
-            return node.value
-        elif (role == Qt.BackgroundRole and index.column() == 3 and
+                return node.value
+        elif (role == Qt.BackgroundRole and index.column() == 4 and
               node.is_updating):
             return self._red_brush
 
         return None
-
-    def setData(self, index, value, role):
-        """Re-implemented function called when a value has been edited"""
-        node = index.internalPointer()
-        if role == Qt.EditRole:
-            new_val = str(value)
-            # This will not update the value, only trigger a setting and
-            # reading of the parameter from the Crazyflie
-            node.set_value(new_val)
-            return True
-        return False
 
     def flags(self, index):
         """Re-implemented function for getting the flags for a certain index"""
@@ -247,9 +214,6 @@ class ParamBlockModel(QAbstractItemModel):
         if not self._enabled:
             return Qt.NoItemFlags
 
-        node = index.internalPointer()
-        if index.column() == 3 and node.parent and node.access == "RW":
-            flag |= Qt.ItemIsEditable
         return flag
 
     def reset(self):
@@ -317,6 +281,82 @@ class ParamTab(Tab, param_tab_class):
 
         self.paramTree.setModel(self.proxyModel)
         self.paramTree.header().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.paramTree.selectionModel().selectionChanged.connect(self._paramChanged)
+
+    def _param_default_cb(self, name, default_value):
+        self.currentValue.setText(self.cf.param.get_value(name))
+        self.defaultValue.setText(str(default_value))
+
+    def _persistent_button_cb(self, _):
+        def success_cb(name, success):
+            if success:
+                self.cf.param.persistent_get_state(name, self._persistent_state_cb)
+
+        complete = self.paramDetailsLabel.text()
+        if self.persistentButton.text() == 'Clear':
+            self.cf.param.persistent_clear(complete, success_cb)
+        else:
+            self.cf.param.persistent_store(complete, success_cb)
+
+    def _persistent_state_cb(self, name, state):
+        self.persistentFrame.setVisible(True)
+        try:
+            self.persistentButton.clicked.disconnect()
+        except Exception:
+            pass
+
+        if state.is_stored:
+            self.storedValue.setText(str(state.stored_value))
+        else:
+            self.storedValue.setText('Not stored')
+
+        self.persistentButton.setText('Clear' if state.is_stored else 'Store')
+        self.persistentButton.clicked.connect(self._persistent_button_cb)
+
+    def _setParamValue(self):
+        name = self.paramDetailsLabel.text()
+        value = self.currentValue.text()
+        self.cf.param.set_value(name, value)
+        self._paramChanged()
+
+    def _paramChanged(self):
+        indexes = self.paramTree.selectionModel().selectedIndexes()
+        selectedIndex = indexes[0]
+
+        self.persistentFrame.setVisible(False)
+
+        param = None
+        if selectedIndex.parent().isValid():
+            group = selectedIndex.parent().data()
+            param = selectedIndex.data()
+        else:
+            group = selectedIndex.data()
+
+        self.paramDetailsLabel.setText(f'{group}.{param}' if param is not None else group)
+        if cfclient.log_param_doc is not None:
+            try:
+                desc = str()
+                group_doc = cfclient.log_param_doc['params'][group]
+                if param is None:
+                    desc = group_doc['desc']
+                else:
+                    desc = group_doc['variables'][param]['short_desc']
+
+                self.paramDetailsDescription.setWordWrap(True)
+                self.paramDetailsDescription.setText(desc.replace('\n', ''))
+            except:  # noqa
+                pass
+
+        self.valueFrame.setVisible(param is not None)
+        if param:
+            complete = f'{group}.{param}'
+            elem = self.cf.param.toc.get_element_by_complete_name(complete)
+            self.cf.param.get_default_value(complete, self._param_default_cb)
+            if elem.is_persistent():
+                self.cf.param.persistent_get_state(complete, self._persistent_state_cb)
+
+            self.setParamButton.clicked.connect(self._setParamValue)
+            self.resetDefaultButton.clicked.connect(lambda: self.currentValue.setText(self.defaultValue.text()))
 
     def _connected(self, link_uri):
         self._model.reset()
