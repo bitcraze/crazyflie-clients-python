69cf114028bac97fcfddadd3686cd00bd78f27a1
knmcguire <kimberly@bitcraze.io>
2022-09-07 17:03:35 +0200

    update cflib to 0.1.20.1

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ setup(
         ],
     },
 
-    install_requires=platform_requires + ['cflib>=0.1.20',
+    install_requires=platform_requires + ['cflib>=0.1.20.1',
                                           'appdirs~=1.4.0',
                                           'pyzmq~=22.3',
                                           'pyqtgraph~=0.11',
