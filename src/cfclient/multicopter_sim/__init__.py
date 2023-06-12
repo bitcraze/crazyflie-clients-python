9440935c469112c25c45f7c5f390a4ff4b5fe1d3
simondlevy <simon.d.levy@gmail.com>
2023-06-11 21:39:39 -0400

    =More MulticopterSim client

--- src/cfclient/multicopter_sim/__init__.py
+++ src/cfclient/multicopter_sim/__init__.py
@@ -16,16 +16,41 @@
 #  Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from sys import stdout
+import socket
 
 class MulticopterSimClient:
 
-    def __init__(self):
+    def __init__(self, host='127.0.0.1', port=5000):
 
-        pass
+        self.host = host
+        self.port = port
 
     def connect(self):
 
-        self._debug('************** CONNECT **************')
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+
+            try:
+
+                sock.connect((self.host, self.port))
+
+                sock.settimeout(0.5)
+
+                while True:
+
+                    try:
+                        telemetry_bytes = sock.recv(8*13)
+
+                    except socket.timeout:
+                        break
+
+                    telemetry = np.frombuffer(telemetry_bytes)
+
+                    self._debug(telemetry)
+
+            except ConnectionRefusedError:
+
+                self._debug(
+                        'Connection error; did you start the server first?')
 
     def _debug(self, msg):
 
