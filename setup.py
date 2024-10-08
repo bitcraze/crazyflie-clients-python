fb47fe9c8342d5f4a7f2bff78202d770e675f19f
Rik Bouwmeester <rikjbouwmeester@gmail.com>
2024-10-08 16:35:24 +0200

    Require Python version >= 3.8

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ from gitversion import get_version
 
 from pathlib import Path
 
-if sys.version_info < (3, 7):
-    raise "must use python 3.7 or greater"
+if sys.version_info < (3, 8):
+    raise "must use python 3.8 or greater"
 
 
 def relative(lst, base=''):
