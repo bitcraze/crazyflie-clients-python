9a5532c26e22056474751d6d8eb275b695196e42
Tove Rumar <tove@bitcraze.io>
2024-11-08 15:50:17 +0100

    Add test for toolbox viewing as well

--- test/test_tabs.py
+++ test/test_tabs.py
@@ -30,3 +30,22 @@ def test_tab_visibility(qtbot,main_window,local):
         test_tab(tab_name)
     if local:
         qtbot.wait(1000)
+
+def test_toolbox_visibility(qtbot,main_window,local):
+    def test_toolbox(name):
+        tab_name = name
+        tab_action_item = next((a for a in main_window.toolboxes_menu_item.actions() if a.text() == tab_name), None)
+        assert tab_action_item is not None, f"Tab action for '{tab_name}' not found"
+
+        assert not tab_action_item.isChecked()
+        tab_action_item.triggered.emit(True)
+        assert tab_action_item.isChecked()
+    # Create an instance of the main UI window
+    qtbot.addWidget(main_window)
+
+    main_window.show()
+    tab_list = ["Console","Parameters","Loco Positioning","Lighthouse Positioning","Crtp sniffer","Log TOC", "LED","Log Blocks","Plotter", "Tuning", "Log Client"]
+    for tab_name in tab_list:
+        test_toolbox(tab_name)
+    if local:
+        qtbot.wait(1000)
