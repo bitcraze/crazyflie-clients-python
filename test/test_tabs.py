f25baee84029d348c539fc405785a5ec2f0e346f
Tove Rumar <tove@bitcraze.io>
2024-11-08 13:31:18 +0100

    When setting up tests clean cache
    We need to import the main window after changing the cache
    directory to a test dir
    If not, much of the config will be fetched while imorting the main window
    Use this setup function to yield the main window to the tests

--- test/test_tabs.py
+++ test/test_tabs.py
@@ -1,15 +1,28 @@
-from PyQt6.QtWidgets import QApplication
-from PyQt6.QtCore import QTimer
 import pytest
-import  cfclient.gui
-from cfclient.ui.main import MainUI
-@pytest.fixture(scope="session")
-def app():
-    return QApplication.instance() or QApplication([])
+import cfclient
+cfclient.config_path  = "test/cache"
 
-def test_main_ui_window(qtbot, app):
+from PyQt6 import QtCore
+from PyQt6.QtWidgets import QTabWidget, QWidget, QTreeWidget, QMenuBar, QMenu
+import cfclient
+import shutil
+import os
+
+
+@pytest.fixture(autouse=True)
+def main_window():
+    cache_dir = cfclient.config_path
+    if os.path.exists(cache_dir):
+        shutil.rmtree(cache_dir)
+    os.makedirs(cache_dir)  # Re-create the empty cache directory
+    from cfclient.ui.main import MainUI #Need to import here to avoid setting up the cache at the wrong place
+    yield MainUI()
+    
+
+
+def test_main_ui_window(qtbot, main_window):
     # Create an instance of the main UI window
-    main_window = MainUI()
+
     qtbot.addWidget(main_window)
 
     # Show the main window and perform assertions
@@ -20,3 +33,22 @@ def test_main_ui_window(qtbot, app):
     qtbot.wait(1000)
     main_window.close()
     assert not main_window.isVisible()
+
+
+def test_tab_visibility(qtbot,main_window):
+    def test_tab(name):
+        tab_name = name
+        tab_action_item = next((a for a in main_window.tabs_menu_item.actions() if a.text() == tab_name), None)
+        assert tab_action_item is not None, f"Tab action for '{tab_name}' not found"
+
+        assert not tab_action_item.isChecked()
+        tab_action_item.triggered.emit(True)
+        assert tab_action_item.isChecked()
+    # Create an instance of the main UI window
+    qtbot.addWidget(main_window)
+
+    main_window.show()
+    tab_list = ["Console","Parameters","Loco Positioning","Lighthouse Positioning","Crtp sniffer","Log TOC", "LED","Log Blocks","Plotter", "Tuning", "Log Client"]
+    for tab_name in tab_list:
+        test_tab(tab_name)
+        qtbot.wait(1000)
