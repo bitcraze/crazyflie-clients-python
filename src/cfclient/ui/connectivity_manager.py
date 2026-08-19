87681250de390467497318168cc1e59ba88e4b84
ArisMorgens <arismorsinkpaloumpas@gmail.com>
2026-08-19 14:42:47 +0200

    Updated copyright references

--- src/cfclient/ui/connectivity_manager.py
+++ src/cfclient/ui/connectivity_manager.py
@@ -7,7 +7,7 @@
 #  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
 #   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
 #
-#  Copyright (C) 2021-2023 Bitcraze AB
+#  Copyright (C) 2021-2026 Bitcraze AB
 #
 #  Crazyflie Nano Quadcopter Client
 #
